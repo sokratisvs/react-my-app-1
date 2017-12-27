687a78551963becd60f1aaf5fc89f83e253da607
sokratisvs <sokratisvs@gmail.com>
2017-12-27 14:41:10 +0200

    React DOM initial commit

--- src/App.js
+++ src/App.js
@@ -25,19 +25,143 @@ class App extends Component {
 }
 
 
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Chart app demo</h1>
-        </header>
+    return [
+    <div className="header">
+       /* callER logo  */
+			 <div className="logo">
+				<img src="images/Logo.png" alt="callER" />
+			 </div>
+			 <a href="#" className="nav-trigger"><span></span></a>
+		 </div>
+    /* side nav bar */
+		<div className="side-nav">
+       	  <div className="logo">
+				     <i className="fa fa-user-circle-o"></i>
+				     <span>Albert Moreno</span>
+			    </div>
+  			<nav>
+        /* Call list item in side bar*/
+				<ul>
+					 <li>
+					    <a href="#">
+							<span ><i className="fa fa-volume-control-phone"></i></span>
+							<span>Call</span>
+						</a>
 
-         <DoughnutChart />
+				  </li>
+        </ul> /* end of Call list item in side bar*/
 
-         <h2>{this.state.Moviedata.title}</h2>
-      </div>
-    );
+				       <p >Analytics</p>    /* Title  */
+          /* list items in side bar */
+				 <ul>
+					 <li>
+					   <a href="#">
+				        <span><i className="fa fa-user iconSize"></i></span>
+				        <span>Accounts</span>
+				      </a>
+					</li>
+					 <li>
+						 <a href="#">
+
+							 <span><i className="fa fa-volume-control-phone iconSize"></i></span>
+							 <span>Campaigns</span>
+						 </a>
+					</li>
+					<li className="active">
+						 <a href="#">
+							  <span><i className="fa fa-users iconSize"></i></span>
+							  <span>Rankings</span>
+					  	</a>
+					</li>
+					<li>
+					 	<a href="#">
+						  	<span><i className="fa fa-lightbulb-o iconSize"></i></span>
+							  <span>Insights</span>
+						 </a>
+					</li>
+				</ul> /* end of list items in side bar*/
+
+			</nav>
+		</div>  /* end of side nav bar */
+
+    /*  main content  div */
+		<div className="main-content">
+			  <div className="title">
+				  Analytics
+			  </div>
+         /* main div  */
+			<div className="main">
+          /*   first widget */
+			  <div className="widget">
+			    	<div className="bigchart"></div>
+				    <div className="wrapper">
+					      <div className="camp-title">Coca Cola Zero Spring Push</div>
+                     <div className="smbox"></div>
+                     <div className="smbox"></div>
+					   </div>
+					  <div className="smwrapper">
+                     <div className="smh3 smh3Margin">Outcome</div>
+					           <div className="smchart smchartMargin"></div>
+					  </div>
+					  <div className="smwrapper">
+                     <div className="smh3">Customer Emotions</div>
+					    <div className="smchart"></div>
+					  </div>
+					<div className="smwrapper smwrapperMargin">
+                     <div className="smh3">Agent Behavior</div>
+					    <div className="smchart"></div>
+					</div>
+				</div>    /* end of first widget */
+
+               /*   second widget */
+        <div className="widget">
+				    <div className="bigchart"></div>
+				    <div className="wrapper">
+					      <div className="camp-title">Coca Cola Light Campaign</div>
+                     <div className="smbox"></div>
+                     <div className="smbox"></div>
+					  </div>
+					  <div className="smwrapper">
+                    <div className="smh3 smh3Margin">Outcome</div>
+					          <div className="smchart smchartMargin"></div>
+					  </div>
+					  <div className="smwrapper">
+                    <div className="smh3">Customer Emotions</div>
+					          <div className="smchart"></div>
+					  </div>
+					  <div className="smwrapper smwrapperMargin">
+                    <div className="smh3">Agent Behavior</div>
+					          <div className="smchart"></div>
+					  </div>
+				</div>   /* end of second widget */
+
+              /*  third widget */
+       <div className="widget">
+			    	<div className="bigchart"></div>
+				    <div className="wrapper">
+					      <div className="camp-title">Acme Inc</div>
+                    <div className="smbox"></div>
+                    <div className="smbox"></div>
+					    </div>
+					    <div className="smwrapper">
+                   <div className="smh3 smh3Margin">Outcome</div>
+					         <div className="smchart smchartMargin"></div>
+					   </div>
+					    <div className="smwrapper">
+                    <div className="smh3">Customer Emotions</div>
+					          <div className="smchart"></div>
+					   </div>
+					   <div className="smwrapper smwrapperMargin">
+                    <div className="smh3">Agent Behavior</div>
+					          <div className="smchart"></div>
+					   </div>
+				</div>  /* end of third widget */
+
+			</div>  /* end of main div */
+		</div>  /* end of main content  */
+  ];
   }
 }
 
